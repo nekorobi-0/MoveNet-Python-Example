1d07a2498e595ea0b6d0cc553b24b1e4aa2b1ff0
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2023-09-07 16:29:37 +0900

    変えた

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,10 @@
 import math
-def data_processing(keypoints_list,scores_list,conf_cam,disp_info):
+def data_processing(keypoints_list,scores_list,conf_cam,disp_info):#画像座標を空間座標に変換
     conf_deg = deg2rad(conf_cam)
-    for i in len(conf_cam):
-        keypoints = keypoints_list[i]
-        keypoints
-def deg2rad(conf_cam):
-    deg_list = [[n*0.0174533 for n in i] for i in conf_cam]
+    for keypoint in len(keypoints_list[1]):#キーポイントの数だけループ
+            #二直線の最接近点を出す
+def deg2rads(conf_cam):#カメラ設定をラジアンに
+    deg_list = [[deg2rad(n) for n in i] for i in conf_cam]
     return deg_list
+def deg2rad(rad):
+    return rad*0.0174533
