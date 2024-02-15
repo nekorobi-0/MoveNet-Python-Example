5da15440a84919f5b854eea5787e90cf9a1903b6
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2024-02-15 16:48:17 +0900

    機能はリア充が爆発してましたね

--- classes.py
+++ classes.py
@@ -1,12 +1,15 @@
+import math
 class cam:
     def __init__(self,w:int,h:int,px:float,py:float,pz:float,deg_h:float,deg_w:float):
-        self.height = h
-        self.width = w
+        self.height:int = h
+        self.width:int = w
         self.origin = pos(px,py,pz)
-        self.deg_h = deg_h
-        self.deg_w = deg_h
+        self.deg_h:float = deg_h
+        self.deg_w:float = deg_h
+        self.h_tanth:float = self.height/math.tan(deg_h)
+        self.w_tanth:float = self.width /math.tan(deg_w)
 class pos:
     def __init__(self,x:float,y:float,z:float):
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x:float = x
+        self.y:float = y
+        self.z:float = z
--- line_to_point.py
+++ line_to_point.py
@@ -1,9 +1,10 @@
 import numpy as np
 import sympy
 import math
+import classes
 #座標系の定義
 #z-upな左手系な座標系とする
-def point_calc(p,v,q,w):
+def point_calc(p:np.ndarray,v:np.ndarray,q:np.ndarray,w:np.ndarray):
     """#直線１を表す点とベクトル
     p = np.array([-1, 1, 1])#位置ベクトル
     v = np.array([1, 0, -1])#方向のベクトル
@@ -62,29 +63,35 @@ def data_prossessing(keypoints_list_list,scores_list_list,cam_info):
             p
              
              
-             
-class kp:#キーポイントとカメラ情報を引数にして位置ベクトルと方向ベクトルを導出するクラス
-        def __init__(self,keypoint,score,cam) -> None:
-            kp =  keypoint
-            sc = score
-            ca = cam
-            pvec = self.ca.pvec
-        def avg(self,other):#もう一方の点との結果を統合して新しくクラスに取り込む
-            pass
-        def calc(self):
-            #ここからキーポイントの座標をカメラの情報と合わせることでカメラ原点のローカル座標系に変換する
-            for i in range(2):
-                math.cos()
-            ldvec = [
-                
-            ]#x,y,z-横がxで縦がy
-            gdvec :np.ndarray = np.array([
-                False,
-                False,
-                False
-            ])*self.ca.rvec#方向ベクトルをカメラの回転行列との積を取ってカメラのローカル座標系を回転を補正したグローバルな方向ベクトルに変換する
-
-
+class twoDpos:
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y
+class kp:#キーポイントとカメラ情報を引数にして位置ベクトルと方向ベクトルを導出するクラス(単体)
+    def __init__(self,keypoint,score,cam) -> None:
+        self.kp:twoDpos =  keypoint
+        self.sc = score
+        self.ca:classes.cam = cam
+        self.pvec = self.ca.pvec
+    def avg(self,other):#もう一方の点との結果を統合して新しくクラスに取り込む
+        pass
+    def calc(self):
+        #ここからキーポイントの座標をカメラの情報と合わせることでカメラ原点のローカル座標系に変換する
+        for i in range(2):
+            math.cos()
+        gdvec :np.ndarray = [
+            
+        ]#x,y,z-横がxで縦がy
+        self.gdvec :np.ndarray = np.array(self.ldvec)*self.ca.rvec
+        #方向ベクトルをカメラの回転行列との積を取ってカメラのローカル座標系を回転を補正したグローバルな方向ベクトルに変換する
+    def calcdeg(self):#角度を導出してそこから方向ベクトルを導出する
+        self.w_deg:float = math.atan2(self.kp.x/self.ca.w_tanth)
+        self.h_deg:float = math.atan2(self.kp.y/self.ca.h_tanth)
+        #x,y,z-横がxで縦がy
+        self.ldvec:np.ndarray = np.array([
+            math.tan(self.w_deg),
+            math.tan(self.h_deg),
+            1])
 
 
 
